1a079527a3e9340b521a7b8b238b7121ccd38583
Aida <128890496+AkagamiNoLorea@users.noreply.github.com>
2024-03-05 19:13:44 +0100

    all origins cors

--- src/main/java/Portfolio/apiControllers/PortfolioController.java
+++ src/main/java/Portfolio/apiControllers/PortfolioController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RequestMapping(path = "api/portfolio")
 public class PortfolioController {
     private final PortfolioService portfolioService;
